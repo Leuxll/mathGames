c9f805937113f5540474b0b3f0f7a16da920b57a
yuefunglee <yuefunglee@cdnis.edu.hk>
2020-03-09 17:26:20 +0800

    - Added Scores.java - Wrote Setter and Getter Methods to prepare to implement users and score funtion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,54 @@ public class Main {
 
     public static User [] userDatabase = new User[10];
 
-    public static void main(String[] args) throws IOException {
+//    public static void main(String[] args) throws IOException {
+//
+//        Scanner options = new Scanner(System.in);
+//        char userInput = ' ';
+//        String output = "";
+//
+//        do {
+//            System.out.println("Welcome to the Math Game");
+//            System.out.println("========================");
+//            System.out.println("1. Start a new game\n2. Display single highest and lowest scores\n3. Display scores sorted from highest to lowest\n4. Display scores sorted from lower to highest\n5. Quit");
+//
+//            userInput = options.next().charAt(0);
+//
+//            if (userInput == '1') {
+//                output = Game.Game(userInput);
+//            } else if (userInput == '2') {
+//                System.out.println("Display single highest and lowest scores");
+//                //output = Scores.Scores(userInput, 10);
+//            } else if (userInput == '3') {
+//                System.out.println("Display scores sorted from highest to lowest");
+//            } else if (userInput == '4') {
+//                System.out.println("Display scores sorted from lowest to highest");
+//            }
+//
+//        } while (userInput != '5');
+//        System.out.println("Thank you for for playing the game!");
+//
+//    }
 
-        Scanner options = new Scanner(System.in);
-        char userInput = ' ';
-        String output = "";
+    public static void main(String[] args) throws IOException{
 
-        do {
+        Scanner scanner = new Scanner(System.in);
+        UserMethods.Init();
+        char choice = '1';
+
+        while (choice != '3') {
             System.out.println("Welcome to the Math Game");
             System.out.println("========================");
-            System.out.println("1. Start a new game\n2. Display single highest and lowest scores\n3. Display scores sorted from highest to lowest\n4. Display scores sorted from lower to highest\n5. Quit");
+            System.out.println("Enter a choice: \n\n1. Existing User\n2. New User\n3. Quit");
+            choice = scanner.nextLine().charAt(0);
 
-            userInput = options.next().charAt(0);
-
-            if (userInput == '1') {
-                output = Game.Game(userInput);
-            } else if (userInput == '2') {
-                System.out.println("Display single highest and lowest scores");
-                //output = Scores.Scores(userInput, 10);
-            } else if (userInput == '3') {
-                System.out.println("Display scores sorted from highest to lowest");
-            } else if (userInput == '4') {
-                System.out.println("Display scores sorted from lowest to highest");
+            if (choice == '1') {
+                System.out.println(UserMethods.AddUser());
             }
+            else if (choice == '2') {
 
-        } while (userInput != '5');
-        System.out.println("Thank you for for playing the game!");
+            }
+        }
 
     }
 }
---
+++ src/com/company/UserMethods.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class UserMethods {
+
+    public static void Init()throws IOException {
+
+        for (int i = 0; i < Main.userDatabase.length; i++) {
+            Main.userDatabase[i] = new User();
+        }
+
+    }
+
+    public static void DisplayUser() {
+
+        for (int i = 0; i < Main.userDatabase.length; i++) {
+
+            if (Main.userDatabase[i].getUserName().equals("-") == false) {
+                System.out.println(Main.userDatabase[i].getUserName());
+                System.out.println(Arrays.toString(Main.userDatabase[i].getScore()));
+            }
+            else {
+                break;
+            }
+
+        }
+
+    }
+
+    public static String AddUser() {
+
+        boolean added = false;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Username: ");
+        String name = scanner.next();
+
+        int score [] = new int [10];
+        for (int i = 0; i < Main.userDatabase.length; i++) {
+
+            if (Main.userDatabase[i].getUserName() != "-") {
+                Main.userDatabase[i] = new User(name, score);
+                added = true;
+                break;
+            }
+
+        }
+        if (added = true) {
+
+            return("City was added");
+
+        }
+        else {
+
+            return("City not added, database full");
+
+        }
+
+    }
+
+}
